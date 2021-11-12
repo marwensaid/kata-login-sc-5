ea7990f331066ec3710eb0ccfd04ff9e06913650
marwen.saidi <marwen.saidi@orange.com>
2021-11-12 15:04:17 +0100

    ok

--- src/main/java/com/howtodoinjava/demo/spring/dao/UserDao.java
+++ src/main/java/com/howtodoinjava/demo/spring/dao/UserDao.java
@@ -1,14 +1,8 @@
 package com.howtodoinjava.demo.spring.dao;
 
-import java.util.List;
-
-import com.howtodoinjava.demo.spring.model.User;
-
 public interface UserDao {
 
    // create a save methode : save user
 
    // create a list methode to list user
-   void save(User user);
-   List<User> list();
 }
